8bf10e77fb27b9ef63925a8af0f8d5511db2673b
bencariazo <30350225+bencariazo@users.noreply.github.com>
2024-04-05 20:16:24 +0800

    Declare variable to store user input numbers

--- js/app.js
+++ js/app.js
@@ -6,6 +6,9 @@ const allBtnNumber = document.querySelectorAll('.btn-number')
 const allBtnOperations = document.querySelectorAll('.btn-operations')
 const allBtnFunctions = document.querySelectorAll('.btn-function')
 
+let previousNum = 0;
+let operator = '';
+let currentNum = 0;
 
 const add = (x,y) => x + y;
 const subtract = (x,y) => x - y;
