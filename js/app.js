306e4cc9d0991c023c6f323dc1c62fa855dbcdf3
bencariazo <30350225+bencariazo@users.noreply.github.com>
2024-04-05 20:11:15 +0800

    Declare variables and assign values of selector

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const calcDisplay = document.querySelector("#calculator-display");
+const clearBtn = document.querySelector('#positiveNegative')
+const allClear = document.querySelector('#allClear')
+const allBtn = document.querySelectorAll('.buttons')
+const allBtnNumber = document.querySelectorAll('.btn-number')
+const allBtnOperations = document.querySelectorAll('.btn-operations')
+const allBtnFunctions = document.querySelectorAll('.btn-function')
+
