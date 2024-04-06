68b042f0c648c12f87a35b8be38e7174f2b8f401
bencariazo <30350225+bencariazo@users.noreply.github.com>
2024-04-06 12:23:27 +0800

    Remove unnecessary variables

--- js/app.js
+++ js/app.js
@@ -1,10 +1,7 @@
 const calcDisplay = document.querySelector("#calculator-display");
-const clearBtn = document.querySelector('#positiveNegative')
-const allClear = document.querySelector('#allClear')
 const allBtn = document.querySelectorAll('.buttons')
 const allBtnNumber = document.querySelectorAll('.btn-number')
 const allBtnOperations = document.querySelectorAll('.btn-operations')
-const allBtnFunctions = document.querySelectorAll('.btn-function')
 const decimalBtn = document.querySelector('#period')
 const positiveNegativeBtn = document.querySelector('#positiveNegative')
 
